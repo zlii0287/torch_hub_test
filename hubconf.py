3362a3f061434affc3bf0dfa426f5742bc10fd77
zlii0287 <100829244+zlii0287@users.noreply.github.com>
2022-11-21 14:15:36 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -7,7 +7,7 @@ def custom_model():
     # initialize model instance
     # load weights from path
     model = mlp.get_training_model()
-    model.load_state_dict(torch.load("/output/model_wt.pth"))
+    model.load_state_dict(torch.load("../output/model_wt.pth"))
     return model
 
 
